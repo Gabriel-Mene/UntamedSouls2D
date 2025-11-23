86efa873446fc476ee7592da5f6a6236ef3e8997
Gabriel Meneghetti <gabriel.meneghetti@sou.unifeob.edu.br>
2025-11-22 23:30:35 -0300

    invetario arrumado porem com 1 erro chato

--- scenes/interface/hud.gd
+++ scenes/interface/hud.gd
@@ -2,17 +2,17 @@ extends CanvasLayer
 
 onready var inventory: Control = get_node("InventoryContainer")
 
+func _ready():
+	inventory.visible = false  # garante que começa fechado
+
 func _process(delta: float) -> void:
-	show_inventory()
-	
-func show_inventory() -> void:
 	if Input.is_action_just_pressed("inventory"):
-		if inventory.is_visible:
-			inventory.is_visible = false
-			inventory.animation.play("hide_container")
-			return
-	
-	if inventory.is_visible == false:
-		inventory.is_visible = true
+		toggle_inventory()
+
+func toggle_inventory() -> void:
+	if inventory.visible:
+		inventory.animation.play("hide_container")
+		inventory.visible = false
+	else:
+		inventory.visible = true
 		inventory.animation.play("show_container")
-		return
