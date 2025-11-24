0e56db03c73f99a2f3f54c542bdf399720f59784
Gabriel Meneghetti <gabriel.meneghetti@sou.unifeob.edu.br>
2025-11-24 00:50:50 -0300

    Correção da inicialcutscene

--- scenes/interface/initial_cutscene.gd
+++ scenes/interface/initial_cutscene.gd
@@ -1,6 +1,17 @@
-extends Control
+extends Control # Ou o tipo de nó raiz da sua cena
 
+# Pré-carrega a próxima cena para a transição
 var next_scene = preload("res://scenes/management/level0.tscn")
 
+# 1. Referência ao nó VideoPlayer (ajuste o caminho se necessário)
+onready var video_player: VideoPlayer = $VideoPlayer
+
+func _ready():
+	# 2. Quando a cena é carregada, inicie a reprodução do vídeo manualmente
+	# Como você já desativou o Autoplay, esta linha é essencial para fazê-lo tocar.
+	video_player.play()
+
+# 3. Esta função é um sinal (signal) conectado.
+# Ela é chamada automaticamente quando o vídeo termina.
 func _on_VideoPlayer_finished():
 	get_tree().change_scene_to(next_scene)
